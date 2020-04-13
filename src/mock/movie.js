161afd4e8bc8583473a624bf207fce505e546662
sne <snezhana.syngur@gmail.com>
2020-04-13 11:42:53 +0200

    Добавляет случайную генерацию строки в описании

--- src/mock/movie.js
+++ src/mock/movie.js
@@ -2,7 +2,8 @@ import {
   getRandomIntegerNumber,
   getRandomArrayItem,
   getRandomNumber,
-  getSeveralRandomItems, convertArrayToString
+  getSeveralRandomItems,
+  convertArrayToString
 } from './../util.js';
 
 const Posters = [`made-for-each-other.png`, `popeye-meets-sinbad.png`, `sagebrush-trail.jpg`, `santa-claus-conquers-the-martians.jpg`, `the-dance-of-life.jpg`, `the-great-flamarion.jpg`, `the-man-with-the-golden-arm.jpg`, `1.jpg`, `2.jpg`, `3.jpg`, `4.jpg`, `5.jpeg`, `6.jpg`, `7.jpg`, `8.png`, `10.jpg`, `11.jpg`, `12.jpg`, `13.jpg`, `14.jpg`];
@@ -17,7 +18,13 @@ const Names = [`Anthony Mann`, `Matthew Graham`, `Ashley Pharoah`, `John Strickl
 
 const Countries = [`USA`, `UK`, `France`, `Spain`, `Canada`];
 
-const Descriptions = [`Lorem ipsum dolor sit amet, consectetur adipiscing elit.`, `Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua`, `Tortor condimentum lacinia quis vel.`, `Pellentesque habitant morbi tristique senectus et netus.`];
+const Descriptions = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
+
+
+const getRandomStringLength = (str) => {
+  let description = str.split('. ').slice(0, getRandomIntegerNumber(1, 5)).join('. ');
+  return description;
+};
 
 const generateMoviesInfo = () => {
   return {
@@ -32,7 +39,7 @@ const generateMoviesInfo = () => {
     runtime: getRandomIntegerNumber(1000, 4000),
     country: convertArrayToString(getSeveralRandomItems(Countries, getRandomIntegerNumber(1, 3))),
     genre: getSeveralRandomItems(Genres, getRandomIntegerNumber(2, 3)),
-    description: getRandomArrayItem(Descriptions),
+    description: getRandomStringLength(Descriptions),
     age: getRandomIntegerNumber(0, 18),
     comments: getRandomIntegerNumber(1, 5),
     isInWatchlist: Math.random() > 0.5,
@@ -47,4 +54,7 @@ const generateMovie = (count) => {
     .map(generateMoviesInfo);
 };
 
-export {generateMoviesInfo, generateMovie};
+export {
+  generateMoviesInfo,
+  generateMovie
+};
